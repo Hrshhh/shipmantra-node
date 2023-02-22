7f47289bea74d936d8373b75d0f9236cc792d0ff
Harsh Dubey <86558899+Hrshhh@users.noreply.github.com>
2023-02-22 18:27:46 +0530

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
-import User from "../models/user";
+import b2b_queries from "../models/user";
 
 export const user = (req, res) => {
     console.log(req.body);
-    const user = new User(req.body);
+    const user = new b2b_queries(req.body);
     console.log("User created ", user);
 }
