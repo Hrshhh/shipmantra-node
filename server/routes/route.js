32a3df47d410cf908a0689a9d88a0f77b72084e4
Harsh Dubey <86558899+Hrshhh@users.noreply.github.com>
2023-02-24 19:33:34 +0530

    Update route.js
    Adding more routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-import { savingQueriesInDatabase } from '../controllers/enquiries';
+import { savingQueriesInDatabase, findWithContactName, findAllUsers, findWithWebsiteName } from '../controllers/enquiries';
 
 router.post('/enquiries', savingQueriesInDatabase);
+router.get('/users', findAllUsers);
+router.get('/enquiries/:contactName', findWithContactName);
+router.get('/enquiries/:websiteName', findWithWebsiteName);
 
 export default router;
